1ddd05d64eb190a6da52caa99a69718edd1969da
EricGeorge <eegee.dev@gmail.com>
2022-02-16 20:23:34 -0800

    refactored and simplified

--- chords.js
+++ chords.js
@@ -1,260 +1,97 @@
-// Triggers chords derived from played note
+/** This script is based off of the Reason Studios Scales and Chords Player.  The settings provide the same outputs
+ * as the similar settings on the Reason Player
+ */
 
+/** TODO:
+ *  - Choose color note properly
+ *  - Add open chord option
+ *  - Rename parameters better
+ *  - Add chord inversions
+ *  - Add scale quantization
+ */
 // Chordtypes
-var unison = [0]
-var fifth = [7];
+const chord = {}
 
-var maj_Chord = [4, 7];
-var min_Chord = [3, 7];
-var dim_Chord = [3, 6];
-var min_sharp5_Chord = [3, 8];
+// intervals
+chord.unison = [0]
+chord.fifth = [7];
 
-var maj7th_Chord = [4, 7, 11];
-var min7th_Chord = [3, 7, 10];
-var dom7th_Chord = [4, 7, 10];
-var dim7th_Chord = [3, 6, 10];
-var min7th_Sharp5_Chord = [3, 8, 10]
+// 3 note chords
+chord.maj = [4, 7];
+chord.min = [3, 7];
+chord.dim = [3, 6];     // not used in Reason Player - min_sharp5 is substituted...
+chord.min_sharp5 = [3, 8];
 
-var maj9th_Chord = [4, 7, 11, 14]
-var min9th_Chord = [3, 7, 10, 14]
-var min7thb9_Chord = [3, 7, 10, 13]
-var dom9th_Chord = [4, 7, 10, 14]
-var min7th_Sharp5_b9 = [3, 8, 10, 13]
+// 4 notes chords
+chord.maj7 = [4, 7, 11];
+chord.min7 = [3, 7, 10];
+chord.dom7 = [4, 7, 10];
+chord.dim7 = [3, 6, 10];    // not used in Reason Player - min_sharp5 is substituted...
+chord.min7_Sharp5 = [3, 8, 10]
+
+// 5 note chords
+chord.maj9 = [4, 7, 11, 14]
+chord.min9 = [3, 7, 10, 14]
+chord.min7b9 = [3, 7, 10, 13]
+chord.dom9 = [4, 7, 10, 14]
+chord.min7_Sharp5_b9 = [3, 8, 10, 13]
+
+// factor this down to 7 notes once I have scale quantization
+var major3Notes = [chord.maj, 0, chord.min, 0, chord.min, chord.maj, 0, chord.maj, 0, chord.min, 0, chord.min_sharp5];
+var minor3Notes = [chord.min, 0, chord.min_sharp5, chord.maj, 0, chord.min, 0, chord.min, chord.maj, 0, chord.maj]
+var major4Notes = [chord.maj7, 0, chord.min7, 0, chord.min7, chord.maj7, 0, chord.dom7, 0, chord.min7, 0, chord.min7_Sharp5];
+var minor4Notes = [chord.min7, 0, chord.min7_Sharp5, chord.maj7, 0, chord.min7, 0, chord.min7, chord.maj7, 0, chord.dom7]
+var major5Notes = [chord.maj9, 0, chord.min9, 0, chord.min7b9, chord.maj9, 0, chord.dom9, 0, chord.min9, 0, chord.min7_Sharp5_b9];
+var minor5Notes = [chord.min9, 0, chord.min7_Sharp5_b9, chord.maj9, 0, chord.min9, 0, chord.min7b9, chord.maj9, 0, chord.dom9]
 
 var activeNotes = [];
 
 function HandleMIDI(event) {
+
+    chordIndex = (event.pitch - GetParameter("Root")) % 12
+    // 1 note
     if (GetParameter("NoteCount") == 0) {
         // unison
         buildChord(event, unison);
     }
+    // 2 notes
     if (GetParameter("NoteCount") == 1) {
         // root + 5th
         buildChord(event, fifth);
     }
+    // 3 notes
     if (GetParameter("NoteCount") == 2) {
         if (GetParameter("Mode") == 0) {
-            // MAJOR
-            switch (event.pitch % 12) {
-                // I
-                case (GetParameter("Root") % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // ii
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // iii
-                case ((GetParameter("Root") + 4) % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // IV
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // V
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // vi
-                case ((GetParameter("Root") + 9) % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // vii°
-                case ((GetParameter("Root") + 11) % 12):
-                    buildChord(event, min_sharp5_Chord);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
-        }
+            buildChord(event, major3Notes[chordIndex])
+            event.trace()
+         }
         else if (GetParameter("Mode") == 1) {
-            // MINOR
-            switch (event.pitch % 12) {
-                // i
-                case (GetParameter("Root") % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // ii°
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min_sharp5_Chord);
-                    break;
-                    // III
-                case ((GetParameter("Root") + 3) % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // iv
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // v
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, min_Chord);
-                    break;
-                    // VI
-                case ((GetParameter("Root") + 8) % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // VII
-                case ((GetParameter("Root") + 10) % 12):
-                    buildChord(event, maj_Chord);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
+            buildChord(event, minor3Notes[chordIndex])
+            event.trace()
         }
     }
+    // 4 notes
     if (GetParameter("NoteCount") == 3) {
         if (GetParameter("Mode") == 0) {
-            // MAJOR
-            switch (event.pitch % 12) {
-                // I7
-                case (GetParameter("Root") % 12):
-                    buildChord(event, maj7th_Chord);
-                    break;
-                    // ii7
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // iii7
-                case ((GetParameter("Root") + 4) % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // IV7
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, maj7th_Chord);
-                    break;
-                    // V7
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, dom7th_Chord);
-                    break;
-                    // vi7
-                case ((GetParameter("Root") + 9) % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // vii°7
-                case ((GetParameter("Root") + 11) % 12):
-                    buildChord(event, min7th_Sharp5_Chord);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
-        } else if (GetParameter("Mode") == 1) {
-            // MINOR
-            switch (event.pitch % 12) {
-                // i7
-                case (GetParameter("Root") % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // ii°7
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min7th_Sharp5_Chord);
-                    break;
-                    // III7
-                case ((GetParameter("Root") + 3) % 12):
-                    buildChord(event, maj7th_Chord);
-                    break;
-                    // iv7
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // v7
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, min7th_Chord);
-                    break;
-                    // VI7
-                case ((GetParameter("Root") + 8) % 12):
-                    buildChord(event, maj7th_Chord);
-                    break;
-                    // VII7
-                case ((GetParameter("Root") + 10) % 12):
-                    buildChord(event, dom7th_Chord);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
+            buildChord(event, major4Notes[chordIndex])
+         }
+        else if (GetParameter("Mode") == 1) {
+            buildChord(event, minor4Notes[chordIndex])
         }
     }
+    // 5 notes
     if (GetParameter("NoteCount") == 4) {
         if (GetParameter("Mode") == 0) {
-            // MAJOR
-            switch (event.pitch % 12) {
-                // I7
-                case (GetParameter("Root") % 12):
-                    buildChord(event, maj9th_Chord);
-                    break;
-                    // ii7
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min9th_Chord);
-                    break;
-                    // iii7
-                case ((GetParameter("Root") + 4) % 12):
-                    buildChord(event, min7thb9_Chord);
-                    break;
-                    // IV7
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, maj9th_Chord);
-                    break;
-                    // V7
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, dom9th_Chord);
-                    break;
-                    // vi7
-                case ((GetParameter("Root") + 9) % 12):
-                    buildChord(event, min9th_Chord);
-                    break;
-                    // vii°7
-                case ((GetParameter("Root") + 11) % 12):
-                    buildChord(event, min7th_Sharp5_b9);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
-        } else if (GetParameter("Mode") == 1) {
-            // MINOR
-            switch (event.pitch % 12) {
-                // i7
-                case (GetParameter("Root") % 12):
-                    buildChord(event, min9th_Chord);
-                    break;
-                    // ii°7
-                case ((GetParameter("Root") + 2) % 12):
-                    buildChord(event, min7th_Sharp5_b9);
-                    break;
-                    // III7
-                case ((GetParameter("Root") + 3) % 12):
-                    buildChord(event, maj9th_Chord);
-                    break;
-                    // iv7
-                case ((GetParameter("Root") + 5) % 12):
-                    buildChord(event, min9th_Chord);
-                    break;
-                    // v7
-                case ((GetParameter("Root") + 7) % 12):
-                    buildChord(event, min7thb9_Chord);
-                    break;
-                    // VI7
-                case ((GetParameter("Root") + 8) % 12):
-                    buildChord(event, maj9th_Chord);
-                    break;
-                    // VII7
-                case ((GetParameter("Root") + 10) % 12):
-                    buildChord(event, dom9th_Chord);
-                    break;
-                    // Default
-                default:
-                    break;
-            }
+            buildChord(event, major5Notes[chordIndex])
+         }
+        else if (GetParameter("Mode") == 1) {
+            buildChord(event, minor5Notes[chordIndex])
         }
     }
 }
 
 function buildChord(root, chordtype) {
+    // play root note
     if (root instanceof NoteOn) {
         var originalNote = new NoteOn(root);
         var record = {
@@ -262,24 +99,28 @@ function buildChord(root, chordtype) {
             events: [originalNote]
         };
         root.send();
+        // play rest of chord notes
         for (var i = 0; i < chordtype.length; i++) {
             var harmony = new NoteOn(root);
             harmony.pitch += chordtype[i];
             record.events.push(harmony);
             harmony.send();
         }
+        // play color note
         if (GetParameter("Color") == 1) {
             var harmony = new NoteOn(root);
             harmony.pitch += chordtype[chordtype.length - 1] + 7
             record.events.push(harmony);
             harmony.send();  
         }
+        // play root note up an octave
         if (GetParameter("UpOctave") == 1) {
             var harmony = new NoteOn(root);
             harmony.pitch += 12
             record.events.push(harmony);
             harmony.send();  
         }
+        // play root note down an octave
         if (GetParameter("DownOctave") == 1) {
             var harmony = new NoteOn(root);
             harmony.pitch -= 12
